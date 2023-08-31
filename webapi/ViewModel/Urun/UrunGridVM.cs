e9a991ee1fd8cd77d9a5544b5d5f939ace51b3b5
ALI OSMAN ALPEREN GURBILEK <ali.osman.alperen.gurbilek@ogrenci.karatay.edu.tr>
2023-08-31 19:32:10 +0300

    System.InvalidOperationException: 'Cannot create a DbSet for 'Urun' because this type is not included in the model for the context.'

---
+++ webapi/ViewModel/Urun/UrunGridVM.cs
@@ -0,0 +1,15 @@
+namespace webapi.ViewModel.Urun
+{
+    public class UrunGridVM
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunFiyat { get; set; }
+        public string UrunKategorisi { get; set; }
+        public string UrunDetay { get; set; }
+        public string UrunKDV { get; set; }
+        public string Birim { get; set; }
+        public string BirimliFiyat { get; set; }
+        public string UrunStok { get; set; }
+    }
+}
